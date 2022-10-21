115e63ca8e7535cf0cb1f4b5943fc6a91c812198
Facundo <facundogds@gmail.com>
2022-10-21 16:41:09 +0000

    Añado fetch, error por resolver

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [inputField, setInput] = useState(""); //Default.
+  const [tasks, setTasks] = useState([]);
+
+  const addTaskHandler = () => {
+    let newTasks = [...tasks];
+    newTasks.push({
+      label: inputField,
+      done: false,
+    });
+	console.log("NewTask");
+	console.log(newTasks);
+    setTasks(newTasks);
+    setInput("");
+    console.log(tasks);
+    fetch("http://assets.breatheco.de/apis/fake/todos/user/facundogds", {
+      method: "PUT",
+      body: JSON.stringify(newTasks),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+	.then((response)=>{
+		console.log("status");
+		console.log(response.status);
+		return response.json();
+	})
+	.then((data) => {
+		console.log("data");
+		console.log(data);
+	})
+	.catch((error) => {
+		console.log("error");
+		console.log(error);
+	});
+  };
+
+  const inputChangeHandler = (event) => {
+    //Seteo continuo.
+    setInput(event.target.value);
+    console.log(event.target.value);
+  };
+
+  return (
+    <>
+      <div className="text-center">
+        <h1>Todo-list</h1>
+        <div>
+          <input
+            onChange={inputChangeHandler}
+            type="text"
+            placeholder="Write a task"
+            value={inputField}
+          />
+          <button onClick={addTaskHandler}>Add task</button>
+        </div>
+      </div>
+      <div className="text-center">
+        <ul>
+          {tasks.map((task, index) => {
+            return <li key={index}>{task}</li>;
+          })}
+        </ul>
+      </div>
+    </>
+  );
 };
 
 export default Home;
