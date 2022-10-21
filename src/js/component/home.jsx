7a82a03f8b7a9df0fc7ecbdefd552e3ab5c14cf9
Facundo <facundogds@gmail.com>
2022-10-21 22:32:10 +0000

    Corrected methods

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,48 +1,62 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
 const Home = () => {
   const [inputField, setInput] = useState(""); //Default.
   const [tasks, setTasks] = useState([]);
-
-  const addTaskHandler = () => {
-    let newTasks = [...tasks];
-    newTasks.push({
+  const addTaskHandler = (e) => {
+    e.preventDefault();
+    let newTasks = [...tasks, {
       label: inputField,
       done: false,
-    });
-	console.log("NewTask");
-	console.log(newTasks);
+    }];
     setTasks(newTasks);
     setInput("");
+    console.log("Task");
     console.log(tasks);
-    fetch("http://assets.breatheco.de/apis/fake/todos/user/facundogds", {
+    fetch("https://assets.breatheco.de/apis/fake/todos/user/facundogds", {
       method: "PUT",
       body: JSON.stringify(newTasks),
       headers: {
         "Content-Type": "application/json",
       },
     })
-	.then((response)=>{
-		console.log("status");
-		console.log(response.status);
-		return response.json();
-	})
-	.then((data) => {
-		console.log("data");
-		console.log(data);
-	})
-	.catch((error) => {
-		console.log("error");
-		console.log(error);
-	});
+  .then((response)=>{
+    console.log("status");
+    console.log(response.status);
+    return response.json();
+  })
+  .then((data) => {
+    console.log("data");
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log("error");
+    console.log(error);
+  });
   };
-
   const inputChangeHandler = (event) => {
     //Seteo continuo.
     setInput(event.target.value);
     console.log(event.target.value);
   };
-
+  useEffect(()=>{
+    fetch("https://assets.breatheco.de/apis/fake/todos/user/facundogds", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+    .then((response)=>{
+      return response.json();
+    })
+    .then((data)=>{
+      setTasks(data);
+      console.log("Task");
+      console.log(tasks);
+    })
+    .catch((error)=>{
+      console.log(error);
+    });
+  }, []);
   return (
     <>
       <div className="text-center">
@@ -60,12 +74,11 @@ const Home = () => {
       <div className="text-center">
         <ul>
           {tasks.map((task, index) => {
-            return <li key={index}>{task}</li>;
+            return <li key={index}>{task.label}</li>;
           })}
         </ul>
       </div>
     </>
   );
 };
-
 export default Home;
